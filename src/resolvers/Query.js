853fc2c50bf0e7f7d84a8782b8a9102587844b71
jamc96 <jamc.mejia@gmail.com>
2020-02-20 14:08:43 -0600

    adding new pokemon attributes

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,14 +1,34 @@
 const axios = require('axios');
 
-async function getPokemons(parent, args, context) {
+async function getAllPokemons() {
     return await axios({
         method: 'get',
         url: 'http://pokeapi.co/api/v2/pokemon'
-    }).then((pokemons)=>{
+    }).then((pokemons)=> {
         return pokemons.data.results
     }).catch((err)=>{
         console.log(err);
     })
+}
+async function getPokemonDetails(url) {
+    const pokemon = await axios({
+        method: 'GET',
+        url: url
+    })
+    return {
+        name: pokemon.data.name,
+        weight: pokemon.data.weight,
+        height: pokemon.data.height,
+        baseExperience: pokemon.data.base_experience,
+        abilities: pokemon.data.abilities.map( x => ({ name: x.ability.name})),
+        moves: pokemon.data.moves.map( x => ({ name: x.move.name })),
+    }
+}
+async function getPokemons(parent, args, context) {
+    const pokemons = await getAllPokemons()
+    const pokemonDetails = pokemons.map(x => getPokemonDetails(x.url))
+    // return all pokemons
+    return await Promise.all(pokemonDetails)
 }
 module.exports = {
     getPokemons,
